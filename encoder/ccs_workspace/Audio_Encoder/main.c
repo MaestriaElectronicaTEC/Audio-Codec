b2cec634ff501726acf744ed32061b2a3f6b2409
Ronny Jimenez <rojiark@gmail.com>
2018-04-20 17:26:03 -0700

    Random generation using DSPLib working

--- encoder/ccs_workspace/Audio_Encoder/main.c
+++ encoder/ccs_workspace/Audio_Encoder/main.c
@@ -4,28 +4,32 @@
 
 #include <dsplib.h>
 
-
-void call_random();
 void call_random()
 {
-	ushort random_return_value; // flag to read if overflow
-	DATA *random_data_array;	// struct where the random numbers will reside
+	rand16init();	// DSPlib init seed routine
 
-	random_return_value = rand16(random_data_array, 100); // create 100 random numbers and store them on random_data_array
+	int random_return_value; 						// flag to read if there is overflow
+	DATA random_data_array[5] = {0, 0, 0, 0, 0};	// struct where the random numbers will reside
+
+	random_return_value = rand16(random_data_array, 5); // create 5 random numbers and store them on random_data_array, using DSPlib
+
+	if(random_return_value) {
+		printf("Return value is bigger than 0 \n");
+	} else {
+		printf("Return value is NOT bigger than 0 \n");
+	}
 
 	int i;
 	for(i = 0; i < 100; i++)
 	{
 		printf("DATA: %i \n", random_data_array[i]);
 	}
+
 }
 
-int main(void) {
-	printf("Hello World!\n");
-
+int main(void)
+{
 	call_random();
-
-
 
 	return 0;
 }
