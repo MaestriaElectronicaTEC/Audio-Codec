e61d440df8c65b34f763e1baebce8a9b267b6668
Ronny Jimenez <rojiark@gmail.com>
2018-04-27 14:03:04 -0700

    Update the Memory Map to allow FFT to be executed

--- encoder/ccs_workspace/Audio_Encoder/main.c
+++ encoder/ccs_workspace/Audio_Encoder/main.c
@@ -2,27 +2,19 @@
 
 #include "fft_routines.h"
 
+#include "t8.h"
+
 #include <dsplib.h>
 
 void call_random()
 {
-	rand16init();	// DSPlib init seed routine
 
-	int random_return_value; 						// flag to read if there is overflow
-	DATA random_data_array[5] = {0, 0, 0, 0, 0};	// struct where the random numbers will reside
-
-	random_return_value = rand16(random_data_array, 5); // create 5 random numbers and store them on random_data_array, using DSPlib
-
-	if(random_return_value) {
-		printf("Return value is bigger than 0 \n");
-	} else {
-		printf("Return value is NOT bigger than 0 \n");
-	}
+	rfft(x, NX, SCALE);
 
 	int i;
-	for(i = 0; i < 100; i++)
+	for (i = 0; i < NX; i++)
 	{
-		printf("DATA: %i \n", random_data_array[i]);
+		printf("DATA: %i \n", x[i]);
 	}
 
 }
